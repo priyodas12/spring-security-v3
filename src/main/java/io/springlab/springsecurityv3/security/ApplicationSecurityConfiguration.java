47b44c9bd03c44dbd7ddf4faeb28c9e6a5261352
Priyobrato Das <priyodas12@gmail.com>
2020-05-25 21:40:14 +0530

    permission based api 3

--- src/main/java/io/springlab/springsecurityv3/security/ApplicationSecurityConfiguration.java
+++ src/main/java/io/springlab/springsecurityv3/security/ApplicationSecurityConfiguration.java
@@ -3,6 +3,7 @@ package io.springlab.springsecurityv3.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -34,18 +35,21 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
                                     .username("abcdf")
                                     .password(passwordEncoder.encode("12345"))
                                     .roles(UserRoles.STUDENT.name())
+                                    .authorities(UserRoles.STUDENT.grantedAuthorities())
                                     .build();
         //admin
         UserDetails adminUser= User.builder()
                                     .username("admin")
                                     .password(passwordEncoder.encode("pass12345"))
                                     .roles(UserRoles.ADMIN.name())
+                                    .authorities(UserRoles.ADMIN.grantedAuthorities())
                                     .build();
         //admin trainee
         UserDetails maxxUser= User.builder()
                                     .username("maxx")
                                     .password(passwordEncoder.encode("password"))
                                     .roles(UserRoles.ADMINTRINEE.name())
+                                    .authorities(UserRoles.ADMINTRINEE.grantedAuthorities())
                                     .build();
 
         return new InMemoryUserDetailsManager(adminUser,commonUser,maxxUser);
@@ -59,7 +63,11 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/","index","/css/*","/js/*").permitAll()
-                .antMatchers("/globalapi/**").hasRole(UserRoles.STUDENT.name())
+                .antMatchers(HttpMethod.DELETE,"/adminapi/**").hasAuthority(UserPermissions.COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.PUT,"/adminapi/**").hasAuthority(UserPermissions.COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.POST,"/adminapi/**").hasAuthority(UserPermissions.COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.GET,"/adminapi/**")
+                .hasAnyRole(UserRoles.ADMIN.name(),UserRoles.ADMINTRINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
--- src/main/java/io/springlab/springsecurityv3/security/UserRoles.java
+++ src/main/java/io/springlab/springsecurityv3/security/UserRoles.java
@@ -1,8 +1,11 @@
 package io.springlab.springsecurityv3.security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public enum UserRoles {
     STUDENT(Sets.newHashSet()),
@@ -21,5 +24,20 @@ public enum UserRoles {
 
     UserRoles(Set<UserPermissions> permissions) {
         this.permissions = permissions;
+    }
+
+    public Set<UserPermissions> getPermissions() {
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> grantedAuthorities(){
+       Set<SimpleGrantedAuthority> permissions
+               =getPermissions()
+               .stream()
+               .map(p-> new SimpleGrantedAuthority(p.getPermission()))
+               .collect(Collectors.toSet());
+       permissions.add(new SimpleGrantedAuthority("ROLE_"+this.name()));
+
+       return permissions;
     }
 }
